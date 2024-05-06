0e489c4d5a6797777229daddab6e7234094bfe91
Rachid Daoudi <112935514+RachidDaoudii@users.noreply.github.com>
2024-05-06 14:24:38 +0000

    section skills

--- constants/skills/Skills.js
+++ constants/skills/Skills.js
@@ -1,11 +1,16 @@
+import { skills } from "@/portfolio";
+
 const Skills = ()=>{
     return (
-        <div className="main" id="skills">
+        <div className="main container" id="skills">
           <div className="skills-header-div">
             <div>
               <h1 className="skills-header">
-                Here's what I do
+                Professional <span className="textSkill">Skills</span>
               </h1>
+              <div className="w-full">
+              {skills && skills?.data[0]?.softwareSkills.map((skill,i)=>(<div className="tech-icons">{skill.skillName}</div>))}
+              </div>
             </div>
           </div>
           
