ae6c0e9e74a4c7f4af51ea480c1699783f578567
Rachid Daoudi <112935514+RachidDaoudii@users.noreply.github.com>
2025-02-18 17:27:22 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,15 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-9DG0EYZRXZ"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-9DG0EYZRXZ');
+      </script>
     </Html>
   );
 }
